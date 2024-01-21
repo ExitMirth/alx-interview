db4d8ef1dfe5e6fa6bcb6f5766b8e7ea4337d340
ExitMirth <maritimglen@gmail.com>
2024-01-21 16:27:03 +0300

    test file

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python3
-""" Minimum Operations
+"""
+Minimum Operations module
+"""
+
+
+def minOperations(n):
     """
+    Calculates the fewest number of operations needed to result in exactly n H
+    characters.
 
+    Args:
+        n (int): Number of H characters to achieve.
 
-def minOperations(n: int) -> int:
-    """ Least amount of operations needed to get n H characters in a row """
-    next = 'H'
-    body = 'H'
-    op = 0
-    while (len(body) < n):
-        if n % len(body) == 0:
-            op += 2
-            next = body
-            body += body
-        else:
-            op += 1
-            body += next
-    if len(body) != n:
+    Returns:
+        int: Fewest number of operations needed, or 0 if impossible.
+    """
+    if n <= 1:
         return 0
-    return op
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
+
+
+if __name__ == "__main__":
+    n = 4
+    print("Min # of operations to reach {} char: {}".format
+          (n, minOperations(n)))
+
+    n = 12
+    print("Min # of operations to reach {} char: {}".format
+          (n, minOperations(n)))
